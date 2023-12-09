735d26bb3289f32693d48468d5404a24171dafbb
TulupaAnton <tulupaanton2@gmail.com>
2023-12-09 17:02:15 +0200

    add configs

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,15 @@
+import CONFIG from "./../config";
+
 export function loadUsers(options) {
   const defaultOptions = {
     page: 1,
     results: 10,
+    baseUrl: CONFIG.BASE_URL,
+    resultsOrder: CONFIG.RESULTS_ORDER,
   };
   const realOptions = { ...defaultOptions, ...options };
 
   return fetch(
-    `https://randomuser.me/api/?results=${realOptions.results}&page=${realOptions.page}&seed=pe2023`
+    `${realOptions.baseUrl}?results=${realOptions.results}&page=${realOptions.page}&seed=${realOptions.resultsOrder}`
   ).then((response) => response.json());
 }
