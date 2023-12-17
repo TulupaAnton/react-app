1ec44b8898629a78eb8da31b6f04641283a20c64
TulupaAnton <tulupaanton2@gmail.com>
2023-12-17 13:58:05 +0200

    Add contact Forms

--- src/components/ContactsForm/index.jsx
+++ src/components/ContactsForm/index.jsx
@@ -1,4 +1,5 @@
-import { Formik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { USER_CONTACTS_VALIDATION_SCHEMA } from "../../utils/validationSchema";
 
 function ContactsForm() {
   const initialValue = {
@@ -11,7 +12,39 @@ function ContactsForm() {
     console.log("values=>>", values);
     formikBag.resetForm();
   };
-  return <Formik initialValues={initialValue} onSubmit={handleSubmit}></Formik>;
+  return (
+    <Formik
+      initialValues={initialValue}
+      onSubmit={handleSubmit}
+      validationSchema={USER_CONTACTS_VALIDATION_SCHEMA}
+    >
+      <Form>
+        <label>
+          <span>Name:</span>
+          <Field type="text" name="userName" placeholder="Name" autoFocus />
+        </label>
+        <ErrorMessage name="userName" />
+
+        <label>
+          <span>Phone number:</span>
+          <Field type="text" name="phoneNumber" placeholder="+380993523868" />
+        </label>
+        <ErrorMessage name="phoneNumber" />
+
+        <label>
+          <span>Email:</span>
+          <Field type="email" name="email" placeholder="your@gmail.com" />
+        </label>
+        <ErrorMessage name="email" />
+        <label>
+          <span>Birthday:</span>
+          <Field type="date" name="birthday" />
+        </label>
+        <ErrorMessage name="birthday" />
+        <button type="submit">Add</button>
+      </Form>
+    </Formik>
+  );
 }
 
 export default ContactsForm;
--- src/utils/validationSchema.js
+++ src/utils/validationSchema.js
@@ -7,3 +7,19 @@ const USER_VALIDATION_SCHEMA = yup.object({
     .matches(/^[A-Z][a-z]*$/)
     .required(),
 });
+
+export const USER_CONTACTS_VALIDATION_SCHEMA = yup.object({
+  userName: yup
+    .string()
+    .min(2)
+    .max(64)
+    .matches(/^[A-Z][a-z]*$/)
+    .required(),
+  phoneNumber: yup
+    .string()
+    .length(13)
+    .matches(/(^\+\d{12}$)/)
+    .required(),
+  email: yup.string().email().required(),
+  birthday: yup.date().max(new Date()),
+});
